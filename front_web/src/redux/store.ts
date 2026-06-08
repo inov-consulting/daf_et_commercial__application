5476177dcc3b1142119908de6e9171bc7c37cbec
Mercure Mekinda <mercuremekinda@gmail.com>
2026-06-08 15:52:50 +0100

    DEDI 31: fix authentification OAuth2.0

--- front_web/src/redux/store.ts
+++ front_web/src/redux/store.ts
@@ -1,34 +1,36 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
+import usersReducer from "./features/users/usersSlice";
+import meReducer from "./features/me/meSlice";
+import companiesReducer from "./features/companies/companiesSlice";
 
 const persistConfig = {
   key: "root",
   storage,
   version: 1,
-  whitelist: [
-    "auth",
-  ],
+  whitelist: ["users", "me"],
 };
 
 const rootReducer = {
-
+  users: usersReducer,
+  me: meReducer,
+  companies: companiesReducer,
 };
 
 const persistedReducer = persistReducer(persistConfig, combineReducers(rootReducer));
 
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: false,
-    }),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
-
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
