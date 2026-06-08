612d1ef31bcf54419f7d76a5b396f9ac265d524d
Vannick Nonongo <vannicknonongo@gmail.com>
2026-06-08 08:49:53 +0100

    fix: gettign user list

--- backend/app/api/v1/schemas/users.py
+++ backend/app/api/v1/schemas/users.py
@@ -41,7 +41,6 @@ class UserOut(BaseModel):
             email=user.email,
             first_name=user.first_name,
             last_name=user.last_name,
-            company_ids=user.company_ids,
             companies=companies or [],
             is_active=user.is_active,
             created_at=user.created_at,
