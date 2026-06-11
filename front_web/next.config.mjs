22a36dbad20a4105eddfde6ea2199296c20e2a48
Mercure Mekinda <mercuremekinda@gmail.com>
2026-06-11 12:20:43 +0100

    DEDI 67: ajout de la dictée vocale au chat

--- front_web/next.config.mjs
+++ front_web/next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
